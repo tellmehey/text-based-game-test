64e607e3d6c942e3b0b55455b774d6cf098a58a4
Tellmehey <141400194+tellmehey@users.noreply.github.com>
2026-02-11 19:59:46 -0600

    Added additional console logging for day events and moved commented out JS code in test.html to a new file, OLD-test.html

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-function repeatEventGen() {
+function NewBeginings() {
+        let inventory = []
+        let GameDay = 0
+        let EnemyHealth = 50
+}
+function YearEventGen() {
     let DayGen = 0
     let DayEvents = []
     let EventCalc = 0
@@ -27,17 +32,21 @@ function ReadDay(n) {
         itemLoot = Math.floor(Math.random * 5)
         if (itemLoot = 1) {
             inventory.push("1") // Material (for buying items)
+            console.log("Found material.")
         }
         else {
             if (itemLoot = 2) {
                 inventory.push("2") // Small Health Potion
+                console.log("Found a small health potion.")
             }
             else {
                 if (itemLoot = 3) {
                     inventory.push("3") // Armor Scraps (for repairing)
+                    console.log("Found a couple of armor scraps.")
                 }
                 else {
-                    inventory.push("4") // Material (for now)
+                    inventory.push("4") // Metal Ingot
+                    console.log("Found a Metal Ingot.")
                 }
             }
         }
@@ -45,10 +54,25 @@ function ReadDay(n) {
     }
     else {
         if (n = 2) {
-            ChallengeSpawn(Math.floor(Math.random * 26) + 50)
+            ChallengeSpawn(Math.floor(Math.random * 26) + 50) 
+            // Base health: 50, Additional health: 0 - 25
+            console.log("Enemy has spawned.")
         }
         else {
             NextDay()
         }
+    }
+}
+function NextDay() {
+    GameDay = GameDay + 1
+    console.log ("A new day begins.")
+}
+function ChallengeSpawn(EnemyHealth) {
+    EnemyHealth =  EnemyHealth // I don't even know...
+}
+function ChallengeDamage(Damage) {
+    EnemyHealth = EnemyHealth - Damage
+    if (EnemyHealth <= 0) {
+        console.log("Enemy has been slained.")
     }
 }
